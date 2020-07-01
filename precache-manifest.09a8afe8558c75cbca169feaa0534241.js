daccbaba0843caca69f15168fc6287ca03b4f14a
Quentin Woivré <quentin.woivre@epitech.eu>
2020-07-01 17:13:53 +0200

    Updates

---
+++ precache-manifest.09a8afe8558c75cbca169feaa0534241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545e2d0d627e52515d1f4b552029bb7c",
+    "url": "/shotgun-player/index.html"
+  },
+  {
+    "revision": "11e3c9f0183e9d271d88",
+    "url": "/shotgun-player/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "900b73c5ada8d883113d",
+    "url": "/shotgun-player/static/js/2.7f407634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shotgun-player/static/js/2.7f407634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e3c9f0183e9d271d88",
+    "url": "/shotgun-player/static/js/main.1ecf0d5c.chunk.js"
+  },
+  {
+    "revision": "c9ea47a30c44b0bfd7e5",
+    "url": "/shotgun-player/static/js/runtime-main.65b1d958.js"
+  }
+]);
