fd6062afc84e135849973f16a39e1059f352a159
Quentin Woivré <quentin.woivre@epitech.eu>
2020-07-02 16:25:36 +0200

    configure-client, ...

--- src/configurations/client/configureClient.js
+++ src/configurations/client/configureClient.js
@@ -1,14 +1,6 @@
 import ApolloClient from "apollo-boost";
 
-// https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
-
-const API_PROXY_URL = "https://cors-anywhere.herokuapp.com/";
-
-const API_URL = "https://spotify-api-graphql-console.herokuapp.com/graphql";
-
 const configureClient = () =>
-  new ApolloClient({
-    uri: `${API_PROXY_URL}${API_URL}`,
-  });
+  new ApolloClient({ uri: process.env.REACT_APP_API_URL });
 
 export default configureClient;
